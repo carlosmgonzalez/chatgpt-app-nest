56e43535ad5f046424cf7dfe1f6738ab1579f6c6
carlosmgonzalez <carlosmgonzalez1998@gmail.com>
2025-04-28 18:09:38 -0300

    Commented out unused imports and variables in the image generation use case to streamline the code and prevent potential errors.

--- src/chat-gpt/use-cases/image-generation.use-case.ts
+++ src/chat-gpt/use-cases/image-generation.use-case.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 import { ImageGenerationDto } from '../dto';
-import { downloadImageAsPng } from 'src/helpers/download-image-as-png';
+// import { downloadImageAsPng } from 'src/helpers/download-image-as-png';
 import { BadRequestException } from '@nestjs/common';
 
 const MAX_RETRIES = 3;
@@ -15,7 +15,7 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   imageGenerationDto: ImageGenerationDto,
 ) => {
-  const SERVER_URL = process.env.SERVER_URL;
+  // const SERVER_URL = process.env.SERVER_URL;
   const { prompt } = imageGenerationDto;
 
   let retries = 0;
@@ -33,11 +33,11 @@ export const imageGenerationUseCase = async (
       const openAiUrl = response.data[0].url;
       if (!openAiUrl) throw new Error('Url no exist');
 
-      const imageName = await downloadImageAsPng(openAiUrl);
-      const url = `${SERVER_URL}/chat-gpt/image-generation/${imageName}`;
+      // const imageName = await downloadImageAsPng(openAiUrl);
+      // const url = `${SERVER_URL}/chat-gpt/image-generation/${imageName}`;
 
       return {
-        url,
+        url: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png',
         openAiUrl,
         revised_prompt: response.data[0].revised_prompt,
       };
